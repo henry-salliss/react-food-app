5ed5b53557ead78f9b238ed8dd8c254c58c923d3
Henry Salliss <Henry.Salliss@bluechipdomain.co.uk>
2021-11-02 17:12:58 +0000

    used useEffect in the headercartbutton module so that a css animation is added each time the number of items in the cart changes

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
@@ -7,12 +7,30 @@ import styles from "./HeaderCartButton.module.css";
 const HeaderCartButton = (props) => {
     const cartContext = useContext(CartContext);
 
+    const [btnAnimated, setBtnAnimated] = useState(false);
+
     const numOfCartItems = cartContext.items.reduce((current, item) => {
         return current + item.amount;
     }, 0);
 
+    const btnClasses = `${styles.button} ${btnAnimated ? styles.bump : ''}`
+
+    useEffect(() => {
+        if (cartContext.items.length === 0) return;
+        setBtnAnimated(true);
+
+        const timer = setTimeout(() => {
+            setBtnAnimated(false)
+        }, 300);
+
+        return () => {
+            clearTimeout(timer)
+        }
+    }, [cartContext.items])
+
+
     return (
-        <button className={styles.button} onClick={props.onClick}>
+        <button className={btnClasses} onClick={props.onClick}>
             <span className={styles.icon}>
                 <CartIcon />
             </span>
